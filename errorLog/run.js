5d439f4e5444299d7b5e71f88d67d9720a8d2b6b
lanyun <yun.lan@istarshine.com>
2024-06-14 09:20:33 +0800

    fix

--- errorLog/run.js
+++ errorLog/run.js
@@ -25,7 +25,7 @@ let run = async function () {
             "bool": {
                 "must": [
                     {
-                        "match_phrase": { "message": "合同退费异常: number不能小于等于0" }
+                        "match_phrase": { "message": "合同退费异常" }
                     }
                 ]
             }
@@ -37,6 +37,7 @@ let run = async function () {
     let r = new RegExp(/\[request_id: (.+)\].+\u9000\u8d39\u8be6\u60c5\u003a\s(\{.+\})/)
     let log_time_r = new RegExp(/^\d{4}\/\d{2}\/\d{2}\s\d{2}:\d{2}:\d{2}/)
 
+    let leg = 1
     for (const item of list) {
         let log_time = item._source.message.match(log_time_r)
         log_time = String(log_time[0]).replace(/\//g, '-')
@@ -65,15 +66,20 @@ let run = async function () {
 
         let multiple = info['_source'].charge_deduct_video_multiple
         let factor = info['_source'].charge_factor
+        let onceFactor = info['_source'].charge_once_factor
 
+        let once = Number(info['_source'].charge_deduct_once)
+        let onceNumber = Number(once).mul(onceFactor).toFixed()
         let videoSize = Number(info['_source'].charge_deduct_video_size)
         let textSize = Number(info['_source'].charge_deduct_size) - videoSize
         let textNumber = new Decimal(textSize).mul(factor).toFixed()
         let videoNumber = new Decimal(videoSize).mul(factor).mul(multiple).toFixed()
         let TotalNumber = new Decimal(textNumber).add(videoNumber).toFixed()
 
+
         let updsl = {
             "doc": {
+                "charge_deduct_once_number": onceNumber,
                 "charge_deduct_number": TotalNumber,
                 "charge_deduct_text_size": textSize,
                 "charge_deduct_video_size": videoSize,
@@ -81,10 +87,25 @@ let run = async function () {
                 "charge_deduct_video_number": videoNumber,
             }
         }
+
+        if (info['_source'].resp_body_code != '200') {
+            updsl = {
+                "doc": {
+                    "charge_deduct_once_number": '0',
+                    "charge_deduct_number": '0',
+                    "charge_deduct_text_size": 0,
+                    "charge_deduct_video_size": 0,
+                    "charge_deduct_text_number": '0',
+                    "charge_deduct_video_number": '0',
+                }
+            }
+        }
+
+
         await updateEs(index, id, updsl)
+        console.log(`处理异常日志进度${leg}/${list.length}`)
+        leg++
     }
-
-
 }
 
 
