c9af366b3baddda430d7ef48a0fd755b90f6a632
lanyun <yun.lan@istarshine.com>
2024-07-04 17:31:00 +0800

    fix：补充sid

--- config/prod.js
+++ config/prod.js
@@ -12,7 +12,17 @@ module.exports = {
             timezone: 'local',
             charset: 'utf8mb4',
             dateStrings: 'date'
-        }
+        },
+        sjpt: {
+            host: 'mysql-xgsj-node1.istarshine.net.cn', //192.168.185.61
+            port: '3306',
+            user: 'root', //puser
+            password: 'dc89u72ptkjy', //1y7b57
+            database: 'gaea',
+            waitForConnections: true,
+            connectionLimit: 5,
+            queueLimit: 0
+        },
     },
     redis: {
         dowding: {
