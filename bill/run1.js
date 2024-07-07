e6898f8934a37c6025497d1e9331388ed9a6978f
ly <448169790@qq.com>
2024-07-07 14:33:28 +0800

    fix：普通没有补偿

--- bill/run1.js
+++ bill/run1.js
@@ -23,57 +23,54 @@ let run = async function () {
     db = db.promise()
 
     try {
-        let times = time.betweenDates('2023-01-01', '2023-04-21', 'YYYYMMDD')
+        //let times = time.betweenDates('2023-01-01', '2023-04-21', 'YYYYMMDD')
 
-        let dir = `${__dirname}/../bill.xlsx`
-        const workbook = XLSX.readFile(dir);
-        const sheetNames = workbook.SheetNames;
-        const sheet = workbook.Sheets[sheetNames[0]];
-        const data = XLSX.utils.sheet_to_json(sheet);
-        let sum = data.length
+        let groupSql = `select contract_id,phone from ly_bill where aim_num is null group by contract_id,phone`
+        let group = await db.query(groupSql)
+        group = group[0]
 
-        let i = 0
 
-        let aim = []
-        for (const item of data) {
-            if (item["合同编号"] == "SJ2022112518" && item["账号"] == "13601035022") {
-                aim.push(item)
+        for (const g of group) {
+
+            let sql = `select * from ly_bill where contract_id='${g.contract_id}' and phone='${g.phone}' order by id,year,month asc`
+            let list = await db.query(sql)
+            list = list[0]
+
+            let lj = '0'
+            for (const item of list) {
+
+                let money_cycle = new Decimal(item.month_num).mul(item.factor).toFixed() //周期消耗金额
+                lj = new Decimal(lj).add(money_cycle).toFixed()
+                let qk = new Decimal(lj).sub(item.money_refund).toFixed()
+
+                let sqlup = `
+            update 
+                ly_bill 
+            set 
+                money_cycle='${money_cycle}',money_sum='${lj}',money_debt='${qk}'
+            where 
+                id=${item.id}
+            `
+                await db.query(sqlup)
+
             }
-        }
-
-        if (aim.length == 0) {
-            console.log("未查询到有效账单");
-            return
-        }
-
-        let lj = "0"
-        for (const item of aim) {
-           
-            let sql1 = `select statistic_data->'$.money_cycle' as money_cycle,statistic_data->'$.money_refund' as money_refund  from contract_bill where id=${item["账单id"]}`
-            let res = await db.query(sql1)
-            res = res[0]
-
-            let money_cycle = res[0].money_cycle ?? "0"
-            let money_refund = res[0]?.money_refund ?? "0"
-
-
-            lj = new Decimal(lj).add(money_cycle).toFixed() //累计消耗金额
-            let qk = new Decimal(lj).sub(money_refund).toFixed()
-            console.log(`${item["账单id"]},累计消费：${lj},累计冲值：${money_refund} 欠款：${qk}`)
-
-            let sql = `
-               update 
-                   contract_bill 
-               set 
-                   statistic_data=JSON_SET(statistic_data,'$.money_sum','${lj}'),statistic_data=JSON_SET(statistic_data,'$.money_debt','${qk}')
-               where 
-                   id=${item["账单id"]}
-               `
-            await db.query(sql)
+            console.log(g.contract_id, g.phone);
         }
 
 
 
+
+
+
+        //     let sql = `
+        //     update 
+        //         contract_bill 
+        //     set 
+        //         statistic_data=JSON_SET(statistic_data,'$.money_sum','${lj}'),statistic_data=JSON_SET(statistic_data,'$.money_debt','${qk}')
+        //     where 
+        //         id=${item["账单id"]}
+        //     `
+        //  await db.query(sql)
 
 
 
