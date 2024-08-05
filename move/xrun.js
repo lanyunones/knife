34f1df9f57393c5c38fe870940264c8133309445
lanyun <yun.lan@istarshine.com>
2024-08-05 13:09:53 +0800

    fix

--- move/xrun.js
+++ move/xrun.js
@@ -26,29 +26,35 @@ let run = async function () {
     sdb = sdb.promise()
 
     try {
+        let suid = 3378691311574016
+        let uid = 9020864
+        let contract_id = 'SJXD2023092720'
+        let factor =new Decimal(new Decimal(10).pow(10)).mul('0.05').toFixed()
+        let token = await sdb.query(`select * from sys_user_info where user_id=${suid}`)
+        
+        
+
 
         let timeArr = betweenDates('2023-09-10', '2024-08-02', 'YYYYMMDD')
         for (const time of timeArr) {
-            let sql = `SELECT sum(total_amount) as total,info_flag FROM sys_interface_status_es where token='ce32aaa7-6f19-4317-9b7e-a755697d2b3d' and ct ='${time}' and info_flag in ('0101','0105','0109','02','03','04','0411','06','07','11','17','21') GROUP BY info_flag`
+            let sql = `SELECT sum(total_amount) as total,info_flag FROM sys_interface_status_es where token='ce32aaa7-6f19-4317-9b7e-a755697d2b3d' and ct ='${time}' and info_flag in ('0101','0105','0109','02','03','04','06','07','11','17','21') GROUP BY info_flag`
             let res = await sdb.query(sql)
             let list = res[0]
-            if(list.length ==0){
+            if (list.length == 0) {
                 continue
             }
-            
-            let sql2 = `SELECT sum(total_amount) as total,info_flag FROM sys_interface_status_es where token='ce32aaa7-6f19-4317-9b7e-a755697d2b3d' and ct ='${time}' and info_flag in ('0101','0105','0109','02','03','04','0411','06','07','11','17','21')`
+
+            let sql2 = `SELECT sum(total_amount) as total,info_flag FROM sys_interface_status_es where token='ce32aaa7-6f19-4317-9b7e-a755697d2b3d' and ct ='${time}' and info_flag in ('0101','0105','0109','02','03','04','06','07','11','17','21')`
             let res2 = await sdb.query(sql2)
-            let total=res2[0][0].total
+            let total = res2[0][0].total
 
-            let uid=9021144
-            let contract_id='SJ2023090103'
-            let factor='66670000'
 
-            let timestr=moment(time).format('YYYY-MM-DD')
 
-            for(const item of list){
-                let number=new Decimal(item.total).mul(factor).toFixed()
-                let sql=`insert into info_flag_bill 
+            let timestr = moment(time).format('YYYY-MM-DD')
+
+            for (const item of list) {
+                let number = new Decimal(item.total).mul(factor).toFixed()
+                let sql = `insert into info_flag_bill 
                 (stime,uid,contract_id,domain,flag,size,number)
                 values
                 ('${timestr}','${uid}','${contract_id}','xsearch','${item.info_flag}',${item.total},'${number}')
@@ -56,15 +62,15 @@ let run = async function () {
                 await db.query(sql)
             }
 
-            let cnumber=new Decimal(total).mul(factor).toFixed()
+            let cnumber = new Decimal(total).mul(factor).toFixed()
 
-            let sqlS=`insert into bill_search 
+            let sqlS = `insert into bill_search 
             (stime,uid,charge_deduct_number,charge_deduct_size,times,name,contract_id,text_size,charge_deduct_text_number)
             values
             ('${timestr}',${uid},${cnumber},${total},1,'xsearch','${contract_id}',${total},${cnumber})
             `
             await db.query(sqlS)
-            
+
             console.log(timestr);
         }
     } catch (error) {
